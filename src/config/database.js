c9d0d1beeeb13917d4fe6ca8ec46d3cc32357985
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2024-08-30 12:10:26 +0600

    chore: Update database configuration to use environment variable

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,7 @@ const mongoose = require("mongoose");
 const connectDB = () => {
   mongoose
     .connect(
-      "mongodb+srv://admin:tushar@cluster0.bstsr.mongodb.net/message-me",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
+      process.env.DATABASE_URL,
     )
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.error("Could not connect to MongoDB:", err));
